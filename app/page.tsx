1bf0477fa16f601cd2b64d89dc2c899159a81c1e
ROHAN HUDATI <rohanabcd0305@gmail.com>
2026-01-28 23:53:22 +0530

    add navigation components and implement coin overview with trending coins feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import React, { Suspense } from 'react';
+import CoinOverview from '@/components/home/CoinOverview';
+import TrendingCoins from '@/components/home/TrendingCoins';
+import {
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from '@/components/home/fallback';
+import Categories from '@/components/home/Categories';
 
-
-const page = () => {
+const Page = async () => {
   return (
-    <div></div>
-  )
-}
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-export default page
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
